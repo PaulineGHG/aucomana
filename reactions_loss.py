2484197f9b512a2f0956f1eb8cba19d3515360cc
paulinehg <octavia.phg@gmail.com>
2022-02-15 12:30:41 +0100

    add type

--- reactions_loss.py
+++ reactions_loss.py
@@ -1,17 +1,18 @@
 import pandas as pd
+from typing import Dict, List, Tuple
 
 
 class Reactions:
 
-    def __init__(self, file_reactions_tsv, species_list=None):
+    def __init__(self, file_reactions_tsv: str, species_list: List[str] = None):
         self.species_list = species_list
         self.data_reactions, \
-            self.data_genes_assoc,\
+            self.data_genes_assoc, \
             self.reactions_list = self.__init_data(file_reactions_tsv)
         self.nb_reactions, self.nb_species = self.data_reactions.shape
         self.reactions_loss = self.__init_reactions_loss()
 
-    def __init_data(self, file_reactions_tsv):
+    def __init_data(self, file_reactions_tsv: str):
         data = pd.read_csv(file_reactions_tsv, sep="\t", header=0, index_col='reaction')
         if self.species_list is None:
             self.__generate_species_list(data)
@@ -29,7 +30,7 @@ class Reactions:
                 break
             self.species_list.append(x)
 
-    def __get_filtered_reactions(self, data_all_reactions, out=1):
+    def __get_filtered_reactions(self, data_all_reactions, out: int = 1):
         nb_species = len(self.species_list)
         filtered_reactions = []
         for reaction in data_all_reactions.index:
@@ -41,7 +42,7 @@ class Reactions:
                 filtered_reactions.append(reaction)
         return filtered_reactions
 
-    def __get_reactions_loss_1_species(self, species):
+    def __get_reactions_loss_1_species(self, species: str):
         loss = []
         for reaction in self.reactions_list:
             if self.data_reactions[species][reaction] == 0:
@@ -54,7 +55,7 @@ class Reactions:
             loss_all[species] = self.__get_reactions_loss_1_species(species)
         return loss_all
 
-    def get_genes_assoc(self, reactions_list=None):
+    def get_genes_assoc(self, reactions_list: List[str] = None):
         genes_assoc = {}
         for species in self.species_list:
             genes_assoc[species] = {}
@@ -66,17 +67,17 @@ class Reactions:
                     str(self.data_genes_assoc[species + "_genes_assoc (sep=;)"][reaction]).split(";")
         return genes_assoc
 
-    def get_common_reactions(self, data_to_compare, specie):
-        reaction_list_self = self.reactions_loss[specie][1]
-        common_reac = []
+    def get_common_reactions(self, data_to_compare, species: str):
+        reaction_list_self = self.reactions_loss[species][1]
+        common_reactions = []
         for r in reaction_list_self:
-            if r in data_to_compare.reactions_loss[specie][1]:
-                common_reac.append(r)
-        return len(common_reac), common_reac
+            if r in data_to_compare.reactions_loss[species][1]:
+                common_reactions.append(r)
+        return len(common_reactions), common_reactions
 
     @staticmethod
-    def print_genes_assoc(dict_genes_assoc):
+    def print_genes_assoc(dict_genes_assoc: Dict[str, Dict[str, str]]):
         for species, gene_assoc in dict_genes_assoc.items():
-            print(f"\n{species} :\n{'='*50}")
+            print(f"\n{species} :\n{'=' * 50}")
             for reaction, genes in gene_assoc.items():
                 print(f"{reaction} : {genes}")
