288305875c6dc0b901514d70df4953cffe71aef9
PaulineHG <octavia.phg@gmail.com>
2022-02-23 09:57:10 +0100

    update files names

--- alage_project.py
+++ alage_project.py
@@ -15,17 +15,17 @@ def reactions_from_file(file):
 
 # 40 : run 40+7
 # 01 : run Pauline all species
-# 0A : run Alexandre
-# 02 : run Pauline, Alexandre like exactly
-# 03 : run Pauline, Alexandre like improved
+# A0 : run Alexandre
+# A1 : run Pauline, Alexandre like exactly
+# A2 : run Pauline, Alexandre like improved
 
 # ### FILES ####
 
-DATA_FILE_01 = "data/run1_reactions.tsv"
+DATA_FILE_01 = "data/run01_reactions.tsv"
 DATA_FILE_40 = 'data/run40_reactions.tsv'
-DATA_FILE_0A = 'data/reactions_phaeo2.tsv'
-DATA_FILE_02 = 'data/run2_reactions.tsv'
-DATA_FILE_03 = 'data/run3_reactions.tsv'
+DATA_FILE_A0 = 'data/runA0_reactions.tsv'
+DATA_FILE_A1 = 'data/runA1_reactions.tsv'
+DATA_FILE_A2 = 'data/runA2_reactions.tsv'
 DATA_LELSB_LOSSES = "data/Lelsb_losses.ods"
 
 # ### Select species ###
@@ -52,9 +52,9 @@ LAMINARIONEMA_E = 'Laminarionema_elsbetiae'
 
 R01 = Reactions(DATA_FILE_01)
 R40 = Reactions(DATA_FILE_40, BROWN_ALGAE_40)
-R0A = Reactions(DATA_FILE_0A)
-R02 = Reactions(DATA_FILE_02)
-R03 = Reactions(DATA_FILE_03)
+RA0 = Reactions(DATA_FILE_A0)
+RA1 = Reactions(DATA_FILE_A1)
+RA2 = Reactions(DATA_FILE_A2)
 
 reac_lostA = reactions_from_file(DATA_LELSB_LOSSES)
 
@@ -63,15 +63,15 @@ reac_lostA = reactions_from_file(DATA_LELSB_LOSSES)
 
 print(R40.reactions_loss[LAMINARIONEMA_E])
 print(R01.reactions_loss[LAMINARIONEMA_E])
-print(R0A.reactions_loss[LAMINARIONEMA_E])
-print(R02.reactions_loss[LAMINARIONEMA_E])
-print(R03.reactions_loss[LAMINARIONEMA_E])
+print(RA0.reactions_loss[LAMINARIONEMA_E])
+print(RA1.reactions_loss[LAMINARIONEMA_E])
+print(RA2.reactions_loss[LAMINARIONEMA_E])
 
 
 # ### Common Reactions ###
 
-print(Reactions.get_common_reactions([R01, R40, R0A], LAMINARIONEMA_E, output_file=True))
-print(Reactions.get_common_reactions([R01, R40, R02], LAMINARIONEMA_E, output_file=True))
-print(Reactions.get_common_reactions([R01, R40, R03], LAMINARIONEMA_E, output_file=True))
+print(Reactions.get_common_reactions([R01, R40, RA0], LAMINARIONEMA_E, output_file=True))
+print(Reactions.get_common_reactions([R01, R40, RA1], LAMINARIONEMA_E, output_file=True))
+print(Reactions.get_common_reactions([R01, R40, RA2], LAMINARIONEMA_E, output_file=True))
 
 
