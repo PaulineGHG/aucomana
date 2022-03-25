e0e412def2e12acad681cac22975d65698bf70a5
paulinehg <octavia.phg@gmail.com>
2022-03-25 11:36:44 +0100

    create init_analysis.py

---
+++ init_analysis.py
@@ -0,0 +1,49 @@
+import os
+import shutil
+
+
+def create_folders(path_study):
+    arbo = [['input_data'], ['output_data'],
+            ['input_data', 'pathways_data'],
+            ['input_data', 'reactions_data'],
+            ['input_data', 'genes_data'],
+            ['input_data', 'metabolites_data'],
+            ['output_data', 'pathways_data'],
+            ['output_data', 'reactions_data'],
+            ['output_data', 'reactions_data', 'common_reac'],
+            ['output_data', 'reactions_data', 'genes_assoc'],
+            ['output_data', 'genes_data'],
+            ['output_data', 'metabolites_data'],
+            ['output_data', 'pages']]
+    for folder in arbo:
+        folder_path = os.path.join(path_study, *folder)
+        print(f'creating folder {folder_path}')
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+
+
+def fill_input_auto(path_study, path_runs, runs=None):
+    for run in os.listdir(path_runs):
+        analysis = os.path.join(path_runs, run, 'analysis')
+        if os.path.exists(analysis):
+            for grp in os.listdir(analysis):
+                if grp != 'group_template.tsv':
+                    analysis_grp = os.path.join(analysis, grp)
+                    nw_obj_l = ['reactions', 'pathways', 'genes', 'metabolites']
+                    for nw_obj in nw_obj_l:
+                        copy_file_tsv(path_study, run, analysis_grp, grp, nw_obj)
+
+
+def copy_file_tsv(path_study, run, analysis_grp, grp, nw_obj):
+    r_file = os.path.join(analysis_grp, f'{nw_obj}.tsv')
+    r_dest = os.path.join(path_study, 'input_data', f'{nw_obj}_data', f'{run}_{grp}_{nw_obj}.tsv')
+    if os.path.exists(r_file):
+        shutil.copy(r_file, r_dest)
+        print(f'copy {r_file} in {r_dest}')
+
+
+path_studyhi = '/home/paulinehg/Documents/test_folder'
+p_r = '/home/paulinehg/Documents/Aucome_runs'
+
+create_folders(path_studyhi)
+fill_input_auto(path_studyhi, p_r)
