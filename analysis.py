9379d3d2bc0462c2316b59203e0b313bb4f4f1d6
PaulineGHG <octavia.phg@gmail.com>
2022-04-19 09:57:53 +0200

    complete analysis global : compare

--- analysis.py
+++ analysis.py
@@ -55,46 +55,52 @@ R40 = "run40"
 # print(PATHWAYS[R01].get_pw_complete([SLAT, PLAC], unique=True))
 
 
-SHORT_READS = get_cat_l("data/runs/run04/analysis/all/reactions.tsv", ORG_TSV, ("SR", 2))
-LONG_READS = get_cat_l("data/runs/run04/analysis/all/reactions.tsv", ORG_TSV, ("LR", 2))
-
-
-def compare_groups(run, group1, group2):
+def compare_groups(run, group1, group2, org_file):
     to_calculate = ("nb_genes", "nb_rnx", "nb_pw > 80%", "nb_pw 100%")
     stat = (" mean", " med", " sd", " min", " max")
     path = f"{PATH_RUNS}/{run}/analysis/all/"
-    R_G1 = Reactions(f"{path}reactions.tsv", group1)
-    R_G2 = Reactions(f"{path}reactions.tsv", group2)
-    P_G1 = Pathways(f"{path}pathways.tsv", group1)
-    P_G2 = Pathways(f"{path}pathways.tsv", group2)
-    G_G1 = Genes(f"{path}genes.tsv", group1)
-    G_G2 = Genes(f"{path}genes.tsv", group2)
+
+    group1_list = get_cat_l(f"{path}reactions.tsv", org_file, group1)
+    group2_list = get_cat_l(f"{path}reactions.tsv", org_file, group2)
+
+    r_g1 = Reactions(f"{path}reactions.tsv", group1_list)
+    r_g2 = Reactions(f"{path}reactions.tsv", group2_list)
+    p_g1 = Pathways(f"{path}pathways.tsv", group1_list)
+    p_g2 = Pathways(f"{path}pathways.tsv", group2_list)
+    g_g1 = Genes(f"{path}genes.tsv", group1_list)
+    g_g2 = Genes(f"{path}genes.tsv", group2_list)
 
     g1_res_file = "output_data/group1.tsv"
     g2_res_file = "output_data/group2.tsv"
     res_file = "output_data/compare_groups.tsv"
 
-    df_g1 = pd.DataFrame(columns=to_calculate, index=group1)
-    df_g2 = pd.DataFrame(columns=to_calculate, index=group2)
+    df_g1 = pd.DataFrame(columns=to_calculate, index=group1_list)
+    df_g2 = pd.DataFrame(columns=to_calculate, index=group2_list)
     col_stat = []
     for x in to_calculate:
         for y in stat:
             col_stat.append(x + y)
-    df_comp = pd.DataFrame(columns=col_stat, index=["group 1", "group 2"])
+    df_comp = pd.DataFrame(columns=col_stat, index=[group1[0], group2[0]])
 
-    for sp in group1:
-        df_g1.loc[sp, to_calculate[0]] = G_G1.nb_genes_species[sp]
-        df_g1.loc[sp, to_calculate[1]] = R_G1.nb_reactions_sp[sp]
-        df_g1.loc[sp, to_calculate[2]] = P_G1.get_pw_over_treshold(sp, 0.8)[sp][0]
-        df_g1.loc[sp, to_calculate[3]] = P_G1.get_pw_complete(sp, False)[sp][0]
+    for sp in group1_list:
+        df_g1.loc[sp, to_calculate[0]] = g_g1.nb_genes_species[sp]
+        df_g1.loc[sp, to_calculate[1]] = r_g1.nb_reactions_sp[sp]
+        df_g1.loc[sp, to_calculate[2]] = p_g1.get_pw_over_treshold(sp, 0.8)[sp][0]
+        df_g1.loc[sp, to_calculate[3]] = p_g1.get_pw_complete(sp, False)[sp][0]
 
-    for sp in group2:
-        df_g2.loc[sp, to_calculate[0]] = G_G2.nb_genes_species[sp]
-        df_g2.loc[sp, to_calculate[1]] = R_G2.nb_reactions_sp[sp]
-        df_g2.loc[sp, to_calculate[2]] = P_G2.get_pw_over_treshold(sp, 0.8)[sp][0]
-        df_g2.loc[sp, to_calculate[3]] = P_G2.get_pw_complete(sp, False)[sp][0]
+    for sp in group2_list:
+        df_g2.loc[sp, to_calculate[0]] = g_g2.nb_genes_species[sp]
+        df_g2.loc[sp, to_calculate[1]] = r_g2.nb_reactions_sp[sp]
+        df_g2.loc[sp, to_calculate[2]] = p_g2.get_pw_over_treshold(sp, 0.8)[sp][0]
+        df_g2.loc[sp, to_calculate[3]] = p_g2.get_pw_complete(sp, False)[sp][0]
 
-    print(df_g1, df_g2)
+    for grp in [(group1[0], df_g1), (group2[0], df_g2)]:
+        for calc in to_calculate:
+            df_comp.loc[grp[0], calc + stat[0]] = None
+
+    df_g1.to_csv(g1_res_file, sep="\t", index=False)
+    df_g2.to_csv(g2_res_file, sep="\t", index=False)
+    print(df_comp)
 
 
-compare_groups(R04, SHORT_READS, LONG_READS)
+compare_groups(R04, ("SR", 2), ("LR", 2), ORG_TSV)
