2d62af299207c1b79c47b790b0037327e1155298
PaulineGHG <octavia.phg@gmail.com>
2022-04-29 16:08:30 +0200

    ajout boxplot (ça marche pas)

--- test_va.py
+++ test_va.py
@@ -1,5 +1,6 @@
 import scipy.stats
 import analysis_runs
+from analysis_runs.utils import *
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
@@ -7,40 +8,13 @@ import matplotlib.patches as mpatches
 
 
 file = "data/runs/run04/analysis/all/reactions.tsv"
+ORG_FILE = "data/species_group.tsv"
 comp_dir_SR_LR = "output_data/compare_SR_LR/"
 lr = analysis_runs.utils.get_cat_l(file, "data/species_group.tsv", ("LR", 2))
 sr = analysis_runs.utils.get_cat_l(file, "data/species_group.tsv", ("SR", 2))
 brown = analysis_runs.utils.get_cat_l(file, "data/species_group.tsv", ("brown", 1))
 lr = set(lr).intersection(set(brown))
 sr = set(sr).intersection(set(brown))
-
-def get_freq_list(r: analysis_runs.reactions.Reactions):
-    frequency = []
-    for sp in r.species_list:
-        f = sum(r.data_reactions[sp])
-        if f < 5:
-            frequency.append(f)
-    return frequency
-
-
-def get_under_mean(comp_dir):
-    sp_under_mean = []
-    for dir in os.listdir(comp_dir):
-        decomp = dir.split(".")[0].split("_")
-        if decomp[0] == "compare":
-            g1 = decomp[1]
-            g2 = decomp[2]
-
-            dict_comp = pd.read_csv(f"{comp_dir}{dir}", sep="\t", index_col=0).to_dict()
-            dict_g1 = pd.read_csv(f"{comp_dir}{g1}.tsv", sep="\t", index_col=0).to_dict()
-            dict_g2 = pd.read_csv(f"{comp_dir}{g2}.tsv", sep="\t", index_col=0).to_dict()
-
-            mean_g1 = dict_comp["nb_genes mean"][g1]
-            mean_g2 = dict_comp["nb_genes mean"][g2]
-            for sp in dict_g1["nb_genes"].keys():
-                if dict_g1["nb_genes"][sp] < mean_g1:
-                    sp_under_mean.append(sp)
-            return sp_under_mean
 
 
 RM = analysis_runs.reactions.Reactions(file, species_list=brown, out=5)
@@ -70,8 +44,29 @@ def barplot_freq(rnx_obj):
     plt.show()
 
 
-barplot_freq(RM)
+def boxplot_grp_completion_rnx(run, org_file, group1, group2):
+    reaction_file = os.path.join(PATH_RUNS, run, "analysis", "all", "reactions.tsv")
+    g1_l = get_cat_l(reaction_file, org_file, group1)
+    g2_l = get_cat_l(reaction_file, org_file, group2)
+    all_sp = g1_l + g2_l
+    out = len(all_sp)//5
+    freq_g1 = []
+    freq_g2 = []
+    r = Reactions(reaction_file, species_list=all_sp, out=out)
+    for sp, nb in r.nb_reactions_sp.items():
+        if sp in g1_l:
+            freq_g1.append(nb)
+        elif sp in g2_l:
+            freq_g2.append(nb)
+    print(freq_g1, freq_g2)
+    # fig = plt.figure(figsize=(10, 7))
+    # ax = fig.add_axes([0, 0, 1, 1])
+    # ax.boxplot(g1_l)
+    # plt.show()
+
+
+
+
+# barplot_freq(RM)
+boxplot_grp_completion_rnx("run04", ORG_FILE, ("LR", 2), ("SR", 2))
 # print(scipy.stats.chisquare(get_freq_list(RM)))
-
-
-
