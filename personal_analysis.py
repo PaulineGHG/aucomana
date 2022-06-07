720805174bee4bfd77ea5bbc4356edc79eb1b349
PaulineHG <octavia.phg@gmail.com>
2022-06-07 16:02:09 +0200

    script barplot, pathway loss

--- personal_analysis.py
+++ personal_analysis.py
@@ -88,11 +88,83 @@ sr = A.get_grp_l(R04, ("SR", 2))
 # print(np.sqrt(np.var(loss_l)))
 species = PATHWAYS[R04].species_list
 
-loss = PATHWAYS[R04].get_pw_absent(species=species, unique=True)
-loss_l = []
-for sp, val in loss.items():
+abs = PATHWAYS[R04].get_pw_absent(species=species, unique=True)
+abs_sr = []
+abs_lr = []
+abs_all = []
+abs_lam = []
+
+for sp, val in abs.items():
+    print(sp, val[0])
+    abs_all.append(val[0])
     if sp in sr:
-        print(sp, val[0])
-        loss_l.append(val[0])
-print(np.mean(loss_l))
-print(np.sqrt(np.var(loss_l)))
+        abs_sr.append(val[0])
+    if sp in lr:
+        abs_lr.append(val[0])
+    if sp == LELS:
+        abs_lam.append(val[0])
+
+mini = PATHWAYS[R04].get_pw_min(species=species, unique=True)
+min_sr = []
+min_lr = []
+min_all = []
+min_lam = []
+
+for sp, val in mini.items():
+    print(sp, val[0])
+    min_all.append(val[0])
+    if sp in sr:
+        min_sr.append(val[0])
+    if sp in lr:
+        min_lr.append(val[0])
+    if sp == LELS:
+        min_lam.append(val[0])
+
+
+inc = PATHWAYS[R04].get_pw_incomplete(species=species, unique=True)
+inc_sr = []
+inc_lr = []
+inc_all = []
+inc_lam = []
+
+for sp, val in inc.items():
+    print(sp, val[0])
+    inc_all.append(val[0])
+    if sp in sr:
+        inc_sr.append(val[0])
+    if sp in lr:
+        inc_lr.append(val[0])
+    if sp == LELS:
+        inc_lam.append(val[0])
+
+# print(np.mean(loss_l))
+# print(np.sqrt(np.var(loss_l)))
+
+col = ["absent", "minimal", "incomplete"]
+ind = ["all", "LR", "SR", "L.elsb"]
+df = pd.DataFrame(columns=ind, index=col)
+df.loc["absent", "all"] = round(float(np.mean(abs_all)), 2)
+df.loc["absent", "LR"] = round(float(np.mean(abs_lr)), 2)
+df.loc["absent", "SR"] = round(float(np.mean(abs_sr)), 2)
+df.loc["absent", "L.elsb"] = round(float(np.mean(abs_lam)), 2)
+
+df.loc["minimal", "all"] = round(float(np.mean(min_all)), 2)
+df.loc["minimal", "LR"] = round(float(np.mean(min_lr)), 2)
+df.loc["minimal", "SR"] = round(float(np.mean(min_sr)), 2)
+df.loc["minimal", "L.elsb"] = round(float(np.mean(min_lam)), 2)
+
+df.loc["incomplete", "all"] = round(float(np.mean(inc_all)), 2)
+df.loc["incomplete", "LR"] = round(float(np.mean(inc_lr)), 2)
+df.loc["incomplete", "SR"] = round(float(np.mean(inc_sr)), 2)
+df.loc["incomplete", "L.elsb"] = round(float(np.mean(inc_lam)), 2)
+
+print(df)
+ax = df.plot.bar()
+for container in ax.containers:
+    ax.bar_label(container)
+ax.legend(bbox_to_anchor=(1.0, 1), loc='upper left')
+ax.set_ylabel("Number of pathways")
+ax.set_title("Unique absent/minimal/incomplete pathway for L.eslb compared to all/LR/SR groups mean")
+plt.xticks(rotation=0)
+plt.tight_layout()
+plt.show()
