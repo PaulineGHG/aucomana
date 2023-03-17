85efb83f65627af419aa8c1a6114e2a992a9f87b
phamongi <octavia.phg@gmail.com>
2023-03-17 16:38:22 +0100

    test asko intersections/unins...

--- Perso_analysis.py
+++ Perso_analysis.py
@@ -1,6 +1,7 @@
 import os.path
 
 from aucomana.aucomana import GroupsAnalysis, SequencesAnalysis
+from aucomana.utils.reactions import Reactions
 from aucomana.utils.utils import get_grp_set
 
 g_file = 'Runs/run62/analysis/group_template.tsv'
@@ -11,6 +12,7 @@ orders = ['Fucales', 'Ectocarpales', 'Discosporangiales', 'Desmarestiales', 'Til
 
 sp_seq_dir = 'Runs/run62/studied_organisms'
 brown = get_grp_set(g_file, 'Phaeophyceae')
+outgr = get_grp_set(g_file, 'Outgroup')
 
 # GA = GroupsAnalysis(comp_dir, g_file)
 
@@ -18,12 +20,21 @@ brown = get_grp_set(g_file, 'Phaeophyceae')
 # GA.group_reactions_comparison(('LR', 'SR', spha), 'Phaeophyceae')
 # GA.group_supervenn_rxn(groups_comp=orders, fig_size=(64, 48), output='supervenn_run62.png')
 
-mannitol_pw_rxn = ['FRUCTOKINASE-RXN', 'MANNITOL-1-PHOSPHATASE-RXN',
-                   'MANNITOL-2-DEHYDROGENASE-RXN', 'MANNPDEHYDROG-RXN']
-align_output = 'Runs/run62/analysis/Multiple_align/PWY-6531'
-SA = SequencesAnalysis(sp_seq_dir, comp_dir)
-for rxn in mannitol_pw_rxn:
-    out = os.path.join(align_output, rxn)
-    os.mkdir(out)
-    SA.multiple_alignments(rxn, 'Ectocarpus-subulatus', out, list(brown))
+# mannitol_pw_rxn = ['FRUCTOKINASE-RXN', 'MANNITOL-1-PHOSPHATASE-RXN',
+#                    'MANNITOL-2-DEHYDROGENASE-RXN', 'MANNPDEHYDROG-RXN']
+# align_output = 'Runs/run62/analysis/Multiple_align/PWY-6531'
+# SA = SequencesAnalysis(sp_seq_dir, comp_dir)
+# for rxn in mannitol_pw_rxn:
+#     out = os.path.join(align_output, rxn)
+#     os.mkdir(out)
+#     SA.multiple_alignments(rxn, 'Ectocarpus-subulatus', out, list(brown))
+
+A = GroupsAnalysis(group_template=g_file, compare_path=comp_dir)
+RB = Reactions(A.reactions_tsv, species_list=list(brown))
+RO = Reactions(A.reactions_tsv, species_list=list(outgr))
+RB1 = Reactions(A.reactions_tsv, species_list=list(brown), out=1)
+
+print(RB.get_rxn_absent('Laminarionema-elsbetiae', unique=True))
+print(RB.nb_reactions)
+print(RB1.nb_reactions)
 
