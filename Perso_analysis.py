38169720fac84be4adfc379b339bcef15c503d72
phamongi <octavia.phg@gmail.com>
2023-03-22 17:20:52 +0100

    test run62_nf

--- Perso_analysis.py
+++ Perso_analysis.py
@@ -5,7 +5,7 @@ from aucomana.utils.reactions import Reactions
 from aucomana.utils.utils import get_grp_set
 
 g_file = 'Runs/run62/analysis/group_template.tsv'
-comp_dir = 'Runs/run62/analysis/all'
+comp_dir = 'Runs/run62_nf/analysis/all'
 spha = 'Sphacelaria-rigidula_FEMALE'
 orders = ['Fucales', 'Ectocarpales', 'Discosporangiales', 'Desmarestiales', 'Tilopteridales',
           'Ralfsiales', 'Laminariales', 'Sphacelariales', 'Dictyotales', 'Outgroup']
@@ -14,11 +14,11 @@ sp_seq_dir = 'Runs/run62/studied_organisms'
 brown = get_grp_set(g_file, 'Phaeophyceae')
 outgr = get_grp_set(g_file, 'Outgroup')
 
-# GA = GroupsAnalysis(comp_dir, g_file)
+GA = GroupsAnalysis(comp_dir, g_file)
 
-# Auc.group_pathway_completion_comparison(('LR', 'SR', spha), 'Phaeophyceae')
-# GA.group_reactions_comparison(('LR', 'SR', spha), 'Phaeophyceae')
-# GA.group_supervenn_rxn(groups_comp=orders, fig_size=(64, 48), output='supervenn_run62.png')
+#GA.group_pathway_completion_comparison(('LR', 'SR', spha), 'Phaeophyceae')
+#GA.group_reactions_comparison(('LR', 'SR', spha), 'Phaeophyceae')
+GA.group_supervenn_rxn(groups_comp=orders, fig_size=(64, 48), output='supervenn_run62_nf.png')
 
 # mannitol_pw_rxn = ['FRUCTOKINASE-RXN', 'MANNITOL-1-PHOSPHATASE-RXN',
 #                    'MANNITOL-2-DEHYDROGENASE-RXN', 'MANNPDEHYDROG-RXN']
@@ -29,12 +29,12 @@ outgr = get_grp_set(g_file, 'Outgroup')
 #     os.mkdir(out)
 #     SA.multiple_alignments(rxn, 'Ectocarpus-subulatus', out, list(brown))
 
-A = GroupsAnalysis(group_template=g_file, compare_path=comp_dir)
-RB = Reactions(A.reactions_tsv, species_list=list(brown))
-RO = Reactions(A.reactions_tsv, species_list=list(outgr))
-RB1 = Reactions(A.reactions_tsv, species_list=list(brown), out=1)
-
-print(RB.get_rxn_absent('Laminarionema-elsbetiae', unique=True))
-print(RB.nb_reactions)
-print(RB1.nb_reactions)
+# A = GroupsAnalysis(group_template=g_file, compare_path=comp_dir)
+# RB = Reactions(A.reactions_tsv, species_list=list(brown))
+# RO = Reactions(A.reactions_tsv, species_list=list(outgr))
+# RB1 = Reactions(A.reactions_tsv, species_list=list(brown), out=1)
+#
+# print(RB.get_rxn_absent('Laminarionema-elsbetiae', unique=True))
+# print(RB.nb_reactions)
+# print(RB1.nb_reactions)
 
