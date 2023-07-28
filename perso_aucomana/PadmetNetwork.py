b0ddf085cfd8b7bfa7d2ce691e98d3e6afdd21ef
phamongi <octavia.phg@gmail.com>
2023-07-28 11:33:41 +0200

    continue PadmetNetwork.py

--- perso_aucomana/PadmetNetwork.py
+++ perso_aucomana/PadmetNetwork.py
@@ -1,4 +1,5 @@
 from padmet.classes.padmetSpec import PadmetSpec
+from typing import Set
 import time
 start = time.time()
 
@@ -14,9 +15,9 @@ def try_key_assignment(dictionary, key):
 class PadmetNetwork:
 
     def __init__(self, padmet_spec):
-        self.reactions = set()
-        self.compounds = set()
-        self.genes = None
+        self.reactions: Set[Reaction] = set()
+        self.compounds: Set[Compound] = set()
+        self.genes: Set[Gene] = set()
         self.proteins = None
         self.classes = None
         self.pathways = None
@@ -24,22 +25,26 @@ class PadmetNetwork:
 
     def __init_attributes(self, p_spec):
         for node in p_spec.dicOfNode.values():
+            # print(node.type)
             if node.type == 'reaction':
                 self.reactions.add(Reaction(node, try_key_assignment(p_spec.dicOfRelationIn, node.id), p_spec))
             elif node.type == 'compound':
                 self.compounds.add(Compound(node, try_key_assignment(p_spec.dicOfRelationIn, node.id), p_spec))
+            elif node.type == 'gene':
+                self.genes.add(Gene(node, try_key_assignment(p_spec.dicOfRelationIn, node.id), p_spec))
 
 
 class Reaction:
     def __init__(self, rxn_node, rxn_rlt_in, p_spec):
         self.id = rxn_node.id
-        self.name = try_key_assignment(rxn_node.misc, 'COMMON-NAME')
+        self.common_name = try_key_assignment(rxn_node.misc, 'COMMON-NAME')
         self.direction = try_key_assignment(rxn_node.misc, 'DIRECTION')
         self.ec_number = try_key_assignment(rxn_node.misc, 'EC-NUMBER')
+        self.name = set()
         self.is_class = set()
         self.xref = None
-        self.supp_data = set()
-        self.reconstruction_data = set()
+        self.supp_data = dict()
+        self.reconstruction_data = dict()
         self.in_pathways = set()
         self.reactants = set()
         self.products = set()
@@ -47,47 +52,84 @@ class Reaction:
         self.__init_rlt_in(rxn_rlt_in, p_spec)
 
     def __init_rlt_in(self, rxn_rlt_in, p_spec):
-        for rlt in rxn_rlt_in:
-            if rlt.type == 'is_a_class':
-                self.is_class.add(rlt.id_out)
-            elif rlt.type == 'consumes':
-                self.reactants.add(rlt.id_out)
-            elif rlt.type == 'produces':
-                self.products.add(rlt.id_out)
-            elif rlt.type == 'has_xref':
-                self.xref = p_spec.dicOfNode[rlt.id_out].misc
-            elif rlt.type == 'has_reconstructionData':
-                self.reconstruction_data.add(rlt.id_out)
-            elif rlt.type == 'has_suppData':
-                self.supp_data.add(rlt.id_out)
-            elif rlt.type == 'is_in_pathway':
-                self.in_pathways.add(rlt.id_out)
-            elif rlt.type == 'is_linked_to':
-                self.linked_genes.add(rlt.id_out)
+        if rxn_rlt_in is not None:
+            for rlt in rxn_rlt_in:
+                if rlt.type == 'is_a_class':
+                    self.is_class.add(rlt.id_out)
+                elif rlt.type == 'consumes':
+                    self.reactants.add(rlt.id_out)
+                elif rlt.type == 'produces':
+                    self.products.add(rlt.id_out)
+                elif rlt.type == 'has_xref':
+                    self.xref = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_reconstructionData':
+                    self.reconstruction_data[rlt.id_out] = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_suppData':
+                    self.supp_data[rlt.id_out] = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'is_in_pathway':
+                    self.in_pathways.add(rlt.id_out)
+                elif rlt.type == 'is_linked_to':
+                    self.linked_genes.add(rlt.id_out)
+                elif rlt.type == 'has_name':
+                    self.name = self.name.union(set(p_spec.dicOfNode[rlt.id_out].misc['LABEL']))
 
 
 class Compound:
 
-    def __init__(self, cpd_node, rxn_rlt_in, p_spec):
+    def __init__(self, cpd_node, cpd_rlt_in, p_spec):
         self.id = cpd_node.id
         self.smiles = try_key_assignment(cpd_node.misc, 'SMILES')
-        self.name = try_key_assignment(cpd_node.misc, 'COMMON-NAME')
+        self.common_name = try_key_assignment(cpd_node.misc, 'COMMON-NAME')
         self.molecular_weight = try_key_assignment(cpd_node.misc, 'MOLECULAR-WEIGHT')
         self.inchi = try_key_assignment(cpd_node.misc, 'INCHI')
         self.inchi_key = try_key_assignment(cpd_node.misc, 'INCHI-KEY')
+        self.name = set()
         self.is_class = set()
         self.xref = None
-        self.supp_data = set()
-        self.__init_rlt_in(rxn_rlt_in, p_spec)
+        self.supp_data = dict()
+        self.__init_rlt_in(cpd_rlt_in, p_spec)
 
-    def __init_rlt_in(self, rxn_rlt_in, p_spec):
-        for rlt in rxn_rlt_in:
-            if rlt.type == 'is_a_class':
-                self.is_class.add(rlt.id_out)
-            elif rlt.type == 'has_xref':
-                self.xref = p_spec.dicOfNode[rlt.id_out].misc
-            elif rlt.type == 'has_suppData':
-                self.supp_data.add(p_spec.dicOfNode[rlt.id_out].misc)
+    def __init_rlt_in(self, cpd_rlt_in, p_spec):
+        if cpd_rlt_in is not None:
+            for rlt in cpd_rlt_in:
+                if rlt.type == 'is_a_class':
+                    self.is_class.add(rlt.id_out)
+                elif rlt.type == 'has_xref':
+                    self.xref = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_suppData':
+                    self.supp_data[rlt.id_out] = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_name':
+                    self.name = self.name.union(set(p_spec.dicOfNode[rlt.id_out].misc['LABEL']))
+
+
+class Gene:
+
+    def __init__(self, g_node, g_rlt_in, p_spec):
+        self.id = g_node.id
+        self.transcription_direction = try_key_assignment(g_node.misc, 'TRANSCRIPTION-DIRECTION')
+        self.centisome_position = try_key_assignment(g_node.misc, 'CENTISOME-POSITION')
+        self.left_end_position = try_key_assignment(g_node.misc, 'LEFT-END-POSITION')
+        self.right_end_position = try_key_assignment(g_node.misc, 'RIGHT-END-POSITION')
+        self.is_class = set()
+        self.name = set()
+        self.xref = None
+        self.supp_data = dict()
+        self.proteins_coding = set()
+        self.__init_rlt_in(g_rlt_in, p_spec)
+
+    def __init_rlt_in(self, g_rlt_in, p_spec):
+        if g_rlt_in is not None:
+            for rlt in g_rlt_in:
+                if rlt.type == 'is_a_class':
+                    self.is_class.add(rlt.id_out)
+                elif rlt.type == 'has_xref':
+                    self.xref = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_suppData':
+                    self.supp_data[rlt.id_out] = p_spec.dicOfNode[rlt.id_out].misc
+                elif rlt.type == 'has_name':
+                    self.name = self.name.union(set(p_spec.dicOfNode[rlt.id_out].misc['LABEL']))
+                elif rlt.type == 'codes_for':
+                    self.proteins_coding.add(rlt.id_out)
 
 
 class Protein:
@@ -100,17 +142,6 @@ class Protein:
         self.supp_data = None
         self.rxn_catalysing = None
         self.linked_species = None
-
-
-class Gene:
-
-    def __init__(self):
-        self.id = None
-        self.is_class = None
-        self.name = None
-        self.xref = None
-        self.supp_data = None
-        self.proteins_coding = None
 
 
 class Pathway:
@@ -134,11 +165,13 @@ class Class:
         self.supp_data = None
 
 
-PADMET_NW_FILE = '/home/phamongi/Documents/Runs/run62/networks/PADMETs/Ascophyllum-nodosum_MALE.padmet'
+PADMET_NW_FILE = 'Ascophyllum-nodosum_MALE.padmet'
+
+
 P = PadmetSpec(PADMET_NW_FILE)
 N = PadmetNetwork(P)
 
-print([c.supp_data for c in N.compounds])
+print([x.supp_data for x in N.genes])
 
 end = time.time()
 print(end-start)
